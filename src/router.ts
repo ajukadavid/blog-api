473a2ddec6ee74dcbef475b796ec6c1ef50966ed
ithiel883 <ajuka.zephiniah@gmail.com>
2023-01-13 01:36:42 +0100

    built api to view all blog posts

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { createNewPost } from './handlers/posts'
+import { createNewPost, getAllPosts } from './handlers/posts'
 
 const router = Router()
 
-router.get('/posts', (req, res) => {res.json('hiii')}) //get all the posts
+router.get('/posts', getAllPosts) //get all the posts
 router.get('/post/:UserId', () => {}) // get all the posts for one user
 router.put('/post/:UserId/:id', () => {}) //get one particular post
 router.post('/post/:id', createNewPost) // create a post
