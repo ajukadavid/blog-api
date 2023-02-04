57506208ec19ce167efea0ab3bf4b4e2109dbbd3
ithiel883 <ajuka.zephiniah@gmail.com>
2023-02-04 03:36:02 +0100

    successfully configured multer

---
+++ src/modules/image-upload.ts
@@ -0,0 +1,4 @@
+export const handleUpload = (req, res, next) => {
+   return console.log(req.file)
+   next()
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
 import morgan from 'morgan'
 import cors from 'cors'
 import express from 'express'
+import { v2 as cloudinary } from 'cloudinary'
 import { createNewUser, getAllUsers, getUserDetails, signInUser} from './handlers/user'
-import { protect } from './modules/auth'
+import { protect} from './modules/auth'
+import { handleUpload } from './modules/image-upload'
 import  router  from './router'
+import multer  from 'multer'
+const upload = multer({dest: 'uploads/'})
 const app = express()
-app.use(cors())
+cloudinary.config({
+    secure: true
+});
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.post('/image', upload.single('thumbnail'), handleUpload)
 app.post('/signUp', createNewUser)
 app.post('/signIn', signInUser)
 app.get('/users', getAllUsers)
