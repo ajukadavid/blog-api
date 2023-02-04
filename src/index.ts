69f164c430c95e664d4b0315aae8040c8856edc8
ithiel883 <ajuka.zephiniah@gmail.com>
2023-02-04 13:53:26 +0100

    setup of dynamic environment

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-// import config from './config'
+import config from './config'
 
 import app from './server'
 
-const port = 4000
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`)
+app.listen(() => {
+    console.log(`Server running on http://localhost:${config.port}`)
 })
