5f6c09f759f8fc2937974e0f19666f0e4186d0d8
ithiel883 <ajuka.zephiniah@gmail.com>
2023-02-04 03:08:19 +0100

    added db model for images and installed multer

---
+++ prisma/migrations/20230204020039_new_migration/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
