3f75bbc7225811723f797c89654ac4af1987d083
ithiel883 <ajuka.zephiniah@gmail.com>
2023-03-18 04:19:22 +0100

    checking image upload

--- src/services/image-handler.ts
+++ src/services/image-handler.ts
@@ -3,6 +3,7 @@ const parser = new DatauriParser();
 import { v2 as cloudinary } from 'cloudinary'
 
 export const imageHandler = async (val) => {
+  console.log(val)
  const img = parser.format('.png', val.file.buffer)   
    try {
     const result = await cloudinary.uploader.upload(img.content);
